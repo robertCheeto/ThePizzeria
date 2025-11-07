07ba13120a6a4ad9c8b9e768756b77e7a74981a7
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-11-07 11:42:26 -0500

    * began working on adding information to HomeScreen Class * working on adding a try-catch for userInput so the user cannot enter letters/other characters when getting the input

--- src/main/java/com/pluralsight/ui/HomeScreen.java
+++ src/main/java/com/pluralsight/ui/HomeScreen.java
@@ -1,4 +1,45 @@
 package com.pluralsight.ui;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class HomeScreen {
+    private Scanner keyboard = new Scanner(System.in);
+
+    public void displayHome() {
+        boolean isRunning = true;
+        int userInput;
+        homeScreenMenu();
+
+        while (isRunning) {
+            try {
+                userInput = keyboard.nextInt();
+                keyboard.nextLine();
+
+                switch (userInput) {
+                    case 1 -> System.out.println("Starting new order...");
+                    case 0 -> {
+                        System.out.println("Program shutting down...");
+                        keyboard.close();
+                        System.exit(0);
+                    }
+                    default -> System.out.print("\nPlease enter a valid input...\nEnter input here: ");
+                }
+
+            } catch (InputMismatchException e) {
+                System.out.println("Please enter a number...");
+                break;
+            }
+        }
+
+
+    }
+
+    public void homeScreenMenu() {
+        System.out.println("*** Welcome to Spezialetti's Pizzeria ***");
+        System.out.println("1) New Order\n0) Exit");
+        System.out.print("Input option here: ");
+
+    }
+
 }

994b9000b2e7a3f751c34e5a3e152e281d622a68
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-11-07 11:27:37 -0500

    * completed Main Class

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,10 @@
 package com.pluralsight;
 
+import com.pluralsight.ui.HomeScreen;
+
 public class Main {
+    public static void main(String[] args) {
+        HomeScreen menu = new HomeScreen();
+        menu.displayHome();
+    }
 }

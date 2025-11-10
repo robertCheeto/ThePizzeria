0b44ea4904900e6963a1ffe0ec9c62b049d08824
DanielSpezialetti <spezialettidaniel@gmail.com>
2025-11-09 19:59:18 -0500

    * added code to PizzaOrder to allow the program to take in inputs from the PizzaMenu class and generate a new Pizza item that will be put into a receipt at a later time

--- src/main/java/com/pluralsight/ordering/PizzaOrder.java
+++ src/main/java/com/pluralsight/ordering/PizzaOrder.java
@@ -1,9 +1,21 @@
 package com.pluralsight.ordering;
 
 import com.pluralsight.models.Pizza;
+import com.pluralsight.models.Toppings;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PizzaOrder {
 
-    //Pizza pie = new Pizza("Pizza", 10, );
+    static ArrayList<Pizza> pizzaOrders = new ArrayList<>();
+
+    public static Pizza buildPizza(String name, String size, double price, String crustType, List<Toppings> toppings) {
+        Pizza newPizza = new Pizza(name, size, price, crustType, toppings);
+        pizzaOrders.add(newPizza);
+
+        return newPizza;
+    }
+
 
 }

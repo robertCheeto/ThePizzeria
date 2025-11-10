75c1276f650ea725ea4fcc5b89f4ae97fa7daea2
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-11-10 15:06:01 -0500

    * continuing to work on the ReceiptGenerator class * working on adding final variables that will make generating the receipt easier

--- src/main/java/com/pluralsight/ordering/ReceiptGenerator.java
+++ src/main/java/com/pluralsight/ordering/ReceiptGenerator.java
@@ -1,4 +1,24 @@
 package com.pluralsight.ordering;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.time.LocalDateTime;
+
 public class ReceiptGenerator {
+    private static final String FILE_LOCATION = "src/main/receipts";
+    private static LocalDateTime localDateTime = LocalDateTime.now();
+    private static final String RECEIPT_HEADER = "\t*** SPEZIALETTI'S PIZZERIA ***" +
+            "\n" + localDateTime + "\n\n\n";
+    /**
+     * class that will take in data from the cart
+     * and write it to a receipt
+     * receipt needs to call the file the following format
+     * "yyyyMMdd-hhmmss.txt"
+     */
+
+    public void printReceipt(CalculateOrder order) {
+        BufferedWriter bufWriter = new BufferedWriter(new FileWriter(FILE_LOCATION, false));
+        bufWriter.write(String.format());
+    }
+
 }

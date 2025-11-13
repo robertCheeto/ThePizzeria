91c5601d9f3662b6a13a07003d4d3913bfac01d2
DanielSpezialetti <spezialettidaniel@gmail.com>
2025-11-13 08:56:37 -0500

    * oh no

--- src/main/java/com/pluralsight/ordering/ReceiptGenerator.java
+++ src/main/java/com/pluralsight/ordering/ReceiptGenerator.java
@@ -12,35 +12,55 @@ public class ReceiptGenerator {
     private static final LocalDateTime localDateTime = LocalDateTime.now();
     private static final DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyyMMdd-hhmmss");
     private static final String formatDateTime = localDateTime.format(format);
+    private static final String RECEIPT_HEADER = "\t*** SPEZIALETTI'S PIZZERIA ***" +
+            "\n" + formatDateTime + "\n\n";
 
     private static final String FILE_LOCATION = "src/main/receipts/";
     private static final String FILE_NAME = formatDateTime + ".txt";
-    private static final String FILE_PATH = FILE_LOCATION + FILE_NAME;
-    private static final File receipt = new File(FILE_PATH);
-    private static String RECEIPT_HEADER = "\t*** SPEZIALETTI'S PIZZERIA ***" +
-            "\n" + formatDateTime + "\n\n";
+    private static final File FILE_PATH = new File(FILE_LOCATION + FILE_NAME);
+    private static final File FOLDER_PATH = new File(FILE_LOCATION);
+
 
 
     public static void printReceipt(CalculateOrder order) {
-        // need to add a check to see if the receipt folder exists
-        try {
-            BufferedWriter bufWriter = new BufferedWriter(new FileWriter(receipt, false));
-            bufWriter.write(RECEIPT_HEADER);
+        if (FOLDER_PATH.exists()) {
+            try {
+                BufferedWriter bufWriter = new BufferedWriter(new FileWriter(FILE_PATH, false));
+                bufWriter.write(RECEIPT_HEADER);
 
-            for (MenuItem orders : order.getCheckOutCart()) {
-                bufWriter.write(String.format(orders.toString()));
+                for (MenuItem orders : order.getCheckOutCart()) {
+                    bufWriter.write(String.format(orders.toString()));
+                }
+
+                bufWriter.close();
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
+        }
+        else {
+            File newFolder = new File(FILE_LOCATION);
+            newFolder.mkdirs();
 
-            bufWriter.close();
+            try {
+                BufferedWriter bufWriter = new BufferedWriter(new FileWriter(FILE_PATH, false));
+                bufWriter.write(RECEIPT_HEADER);
 
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+                for (MenuItem orders : order.getCheckOutCart()) {
+                    bufWriter.write(String.format(orders.toString()));
+                }
+
+                bufWriter.close();
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
     public static void displayReceipt() {
         try {
-            BufferedReader bufReader = new BufferedReader(new FileReader(receipt));
+            BufferedReader bufReader = new BufferedReader(new FileReader(FILE_PATH));
 
             String input = "";
 

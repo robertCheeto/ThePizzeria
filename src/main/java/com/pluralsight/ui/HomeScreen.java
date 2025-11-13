e8485266257e440c3c1ce6270c3dcc6019a75265
DanielSpezialetti <spezialettidaniel@gmail.com>
2025-11-13 09:50:20 -0500

    * updated method name

--- src/main/java/com/pluralsight/ui/HomeScreen.java
+++ src/main/java/com/pluralsight/ui/HomeScreen.java
@@ -71,7 +71,7 @@ public class HomeScreen {
                     }
                     else {
                         System.out.println("Loading cart...\n");
-                        checkOutCart();
+                        displayCart();
                         Utilities.pressEnterToContinue();
                     }
                 }
@@ -99,7 +99,7 @@ public class HomeScreen {
         }
     }
 
-    public void checkOutCart() {
+    public void displayCart() {
         CalculateOrder.displayCheckoutCart();
     }
 
